0f250b62e1de51b02608e709fcbec87f60958319
pmixer <dreaming_hz@hotmail.com>
2020-09-20 17:28:27 -0400

    simpler, cleaner

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch
 
 
 class PointWiseFeedForward(torch.nn.Module):
-    def __init__(self, hidden_units, dropout_rate): # wried, why fusion X 2?
+    def __init__(self, hidden_units, dropout_rate):
 
         super(PointWiseFeedForward, self).__init__()
 
@@ -18,6 +18,9 @@ class PointWiseFeedForward(torch.nn.Module):
         outputs += inputs
         return outputs
 
+# pls use the following self-made multihead attention layer
+# in case your pytorch version is below 1.16 or for other reasons
+# https://github.com/pmixer/TiSASRec.pytorch/blob/master/model.py
 
 class SASRec(torch.nn.Module):
     def __init__(self, user_num, item_num, args):
@@ -58,50 +61,7 @@ class SASRec(torch.nn.Module):
             # self.pos_sigmoid = torch.nn.Sigmoid()
             # self.neg_sigmoid = torch.nn.Sigmoid()
 
-    def forward(self, user_ids, log_seqs, pos_seqs, neg_seqs): # for training
-        seqs = self.item_emb(torch.LongTensor(log_seqs).to(self.dev)) # user_ids hasn't been used yet
-        seqs *= self.item_emb.embedding_dim ** 0.5
-        positions = np.tile(np.array(range(log_seqs.shape[1])), [log_seqs.shape[0], 1])
-        seqs += self.pos_emb(torch.LongTensor(positions).to(self.dev)) # seems wrong/useless 'positional encoding'
-        seqs = self.emb_dropout(seqs)
-
-        # mask 0th items(placeholder for dry-run) in log_seqs
-        # would be easier if 0th item could be an exception for training
-        timeline_mask = torch.BoolTensor(log_seqs == 0).to(self.dev)
-        seqs *= ~timeline_mask.unsqueeze(-1) # broadcast in last dim
-
-        tl = seqs.shape[1] # time dim len for enforce causality
-        attention_mask = ~torch.tril(torch.ones((tl, tl), dtype=torch.bool, device=self.dev))
-
-        for i in range(len(self.attention_layers)):
-            # Self-attention, Q=layernorm(seqs), K=V=seqs
-            seqs = torch.transpose(seqs, 0, 1) # (N, T, C) -> (T, N, C)
-            Q = self.attention_layernorms[i](seqs) # PyTorch mha requires time first fmt
-            mha_outputs, _ = self.attention_layers[i](Q, seqs, seqs, attn_mask=attention_mask)
-                                            # key_padding_mask=timeline_mask, nan?
-                                            # need_weights=False) this arg do not work?
-            seqs = Q + mha_outputs
-            seqs = torch.transpose(seqs, 0, 1) # (T, N, C) -> (N, T, C)
-
-            # Point-wise Feed-forward, actually 2 Conv1D for channel wise fusion
-            seqs = self.forward_layernorms[i](seqs)
-            seqs = self.forward_layers[i](seqs)
-            seqs *=  ~timeline_mask.unsqueeze(-1)
-
-        log_feats = self.last_layernorm(seqs)
-
-        pos_embs = self.item_emb(torch.LongTensor(pos_seqs).to(self.dev))
-        neg_embs = self.item_emb(torch.LongTensor(neg_seqs).to(self.dev))
-
-        pos_logits = (log_feats * pos_embs).sum(dim=-1)
-        neg_logits = (log_feats * neg_embs).sum(dim=-1)
-
-        # pos_pred = self.pos_sigmoid(pos_logits)
-        # neg_pred = self.neg_sigmoid(neg_logits)
-
-        return pos_logits, neg_logits # pos_pred, neg_pred
-
-    def predict(self, user_ids, log_seqs, item_indices): # for inference
+    def log2feats(self, log_seqs):
         seqs = self.item_emb(torch.LongTensor(log_seqs).to(self.dev))
         seqs *= self.item_emb.embedding_dim ** 0.5
         positions = np.tile(np.array(range(log_seqs.shape[1])), [log_seqs.shape[0], 1])
@@ -119,7 +79,7 @@ class SASRec(torch.nn.Module):
             Q = self.attention_layernorms[i](seqs)
             mha_outputs, _ = self.attention_layers[i](Q, seqs, seqs, 
                                             attn_mask=attention_mask)
-                                            # key_padding_mask=timeline_mask # too bad to have duplicated code
+                                            # key_padding_mask=timeline_mask
                                             # need_weights=False) this arg do not work?
             seqs = Q + mha_outputs
             seqs = torch.transpose(seqs, 0, 1)
@@ -129,6 +89,26 @@ class SASRec(torch.nn.Module):
             seqs *=  ~timeline_mask.unsqueeze(-1)
 
         log_feats = self.last_layernorm(seqs) # (U, T, C) -> (U, -1, C)
+
+        return log_feats
+
+    def forward(self, user_ids, log_seqs, pos_seqs, neg_seqs): # for training        
+        log_feats = self.log2feats(log_seqs) # user_ids hasn't been used yet
+
+        pos_embs = self.item_emb(torch.LongTensor(pos_seqs).to(self.dev))
+        neg_embs = self.item_emb(torch.LongTensor(neg_seqs).to(self.dev))
+
+        pos_logits = (log_feats * pos_embs).sum(dim=-1)
+        neg_logits = (log_feats * neg_embs).sum(dim=-1)
+
+        # pos_pred = self.pos_sigmoid(pos_logits)
+        # neg_pred = self.neg_sigmoid(neg_logits)
+
+        return pos_logits, neg_logits # pos_pred, neg_pred
+
+    def predict(self, user_ids, log_seqs, item_indices): # for inference
+        log_feats = self.log2feats(log_seqs) # user_ids hasn't been used yet
+
         final_feat = log_feats[:, -1, :] # only use last QKV classifier, a waste
 
         item_embs = self.item_emb(torch.LongTensor(item_indices).to(self.dev)) # (U, I, C)
