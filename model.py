30c43cf090d429480339ab18d43354b3e399bc29
pmixer <dreaming_hz@hotmail.com>
2020-10-10 22:11:17 -0400

    forgot the only act func before, now fixed

--- model.py
+++ model.py
@@ -9,11 +9,12 @@ class PointWiseFeedForward(torch.nn.Module):
 
         self.conv1 = torch.nn.Conv1d(hidden_units, hidden_units, kernel_size=1)
         self.dropout1 = torch.nn.Dropout(p=dropout_rate)
+        self.relu = torch.nn.ReLU()
         self.conv2 = torch.nn.Conv1d(hidden_units, hidden_units, kernel_size=1)
         self.dropout2 = torch.nn.Dropout(p=dropout_rate)
 
     def forward(self, inputs):
-        outputs = self.dropout2(self.conv2(self.dropout1(self.conv1(inputs.transpose(-1, -2)))))
+        outputs = self.dropout2(self.conv2(self.relu(self.dropout1(self.conv1(inputs.transpose(-1, -2))))))
         outputs = outputs.transpose(-1, -2) # as Conv1D requires (N, C, Length)
         outputs += inputs
         return outputs
